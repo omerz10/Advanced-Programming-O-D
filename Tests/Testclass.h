2b291a7e212387a1e00c5776defc098c3185901c
Omer <omerz1990@gmail.com>
2017-11-28 21:03:47 +0200

    testing commit

---
+++ Tests/Testclass.h
@@ -0,0 +1,14 @@
+//
+// Created by omerz on 28/11/17.
+//
+
+#ifndef PROJECT_UPDATED_TESTCLASS_H
+#define PROJECT_UPDATED_TESTCLASS_H
+
+
+class Testclass {
+
+};
+
+
+#endif //PROJECT_UPDATED_TESTCLASS_H
